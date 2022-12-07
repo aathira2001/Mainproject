237f8c176b73590a10cc718e44dae8cb792794f0
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2022-12-07 12:12:48 +0530

    Update collection.js

--- dbconnector/collection.js
+++ dbconnector/collection.js
@@ -1,6 +1,5 @@
 module.exports={
-    //collection set is pending due to the front-end integration pending
-    // collection:'collection_name', Example of collection seting
+    USER_COLLECTION:'user'
 
 
 
