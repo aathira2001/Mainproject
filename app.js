59885689e824a0f52e7c9c85c8d44e8620a0a0db
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2023-02-15 10:03:35 +0530

    Update app.js
    reupdated the errors and add some error function such as 404 etc

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-/*Author:Akhil*/
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
  var db=require('./dbconnector/connection');//this is the connection file
+//  var createError=require('createError')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// const { create } = require('hbs');
 
 var app = express();
 
@@ -14,7 +16,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'views')));
+// app.use(express.static(path.join(__dirname, 'views')));
+// server css as static
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
  db.connect((err)=>{
   if(err)
   console.log('\n unable to connect to database(Team vacctrac please check your internet connection is proper or not):)');//if error in connection
@@ -24,5 +31,25 @@ app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+const createError=()=>{
+  console.log("")
+}
+app.use(function(req, res, next) {
+  next(createError(404));
+  
+});
+
+app.get('/404', function(req, res, next){
+next();// trigger a 404 since no other middleware will match /404 after this one, and we're not responding here
+});
+app.get('/403', function(req, res, next){// trigger a 403 error
+  var err = new Error('not allowed!');
+  err.status = 403;
+  next(err);
+});
+app.get('/500', function(req, res, next){// trigger a generic (500) error
+  next(new Error('ERROR!'));
+});
+
 
 module.exports = app;
