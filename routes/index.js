4887aa34f162a4065da1f60d7ee8e36a4fac4cec
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2023-02-15 14:26:35 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 const path=require('path')
 var router = express.Router();
@@ -5,11 +6,39 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-res.sendFile(path.join(__dirname, '../public', '/login.html'))
+res.sendFile(path.join(__dirname, '../public', 'm1/login.html'))
  
 });
-router.post('/test',(req,res)=>{
+
+router.post('/login',function(req,res){
   console.log(req.body)
- 
+
 
 })
+
+router.get('/home',function(req,res){
+  res.sendFile(path.join(__dirname,'../public','m1/home.html'))
+})
+
+router.post('/home',function(req,res){
+
+//no input fetch from the home page
+
+})
+
+router.get('/signup',function(req,res){
+  res.sendFile(path.join(__dirname,'../public','test.html'))
+})
+
+router.post('/signup',function(req,res){
+  console.log(req.body)
+})
+
+
+
+
+
+
+
+
+module.exports = router;
